6d9829da41195d41bbe7e16ff1eb6ad4da7a2043
FabianUM <fabian.israel.07@gmail.com>
2024-07-27 10:23:09 -0500

    ajuste sintaxis login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -39,30 +39,30 @@ export default function Login() {
           </div>
 
           <div className="mb-4 flex">
-            <div class="inline-flex items-center w-full">
+            <div className="inline-flex items-center w-full">
               <label
-                class="relative flex cursor-pointer items-center rounded-full"
-                for="checkbox-6"
+                className="relative flex cursor-pointer items-center rounded-full"
+                htmlFor="checkbox-6"
                 data-ripple-dark="true"
               >
                 <input
                   type="checkbox"
-                  class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-indigo-500 checked:bg-indigo-500 checked:before:bg-indigo-500 hover:before:opacity-10"
+                  className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-indigo-500 checked:bg-indigo-500 checked:before:bg-indigo-500 hover:before:opacity-10"
                   id="checkbox-6"
                 />
-                <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                <div className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-3.5 w-3.5"
+                    className="h-3.5 w-3.5"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                     stroke="currentColor"
-                    stroke-width="1"
+                    strokeWidth="1"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                 </div>
@@ -71,8 +71,8 @@ export default function Login() {
                 Recuerdame
               </span>
             </div>
-            <div class="w-full text-right flex justify-center items-center">
-              <a href="#" class="text-indigo-600 hover:text-indigo-800 transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105">
+            <div className="w-full text-right flex justify-center items-center">
+              <a href="#" className="text-indigo-600 hover:text-indigo-800 transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105">
                 ¿Olvidó su contraseña?
               </a>
             </div>
