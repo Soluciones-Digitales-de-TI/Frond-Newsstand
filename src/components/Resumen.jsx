3033250002438b9c49a745f506e16ce8593f32a5
FabianUM <fabian.israel.07@gmail.com>
2024-07-31 16:54:16 -0500

    calculando total a pagar

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -1,9 +1,10 @@
+import { formatearDinero } from "../helpers";
 import useQuiosco from "../hooks/useQuiosco"
 import ResumenProducto from "./ResumenProducto";
 
 export default function Resumen() {
 
-  const { pedido } = useQuiosco();
+  const { pedido, total } = useQuiosco();
 
   return (
     <aside className="md:w-72 h-screen overflow-y-scroll p-5">
@@ -30,6 +31,7 @@ export default function Resumen() {
 
       <p className="text-xl mt-10">
         Total: {''}
+        {formatearDinero(total)}
       </p>
 
       <form className="w-full">
